57a9ec65186f61dec6e0769b053304bde18acca3
xilei.y <yann.yxl@shopastro-inc.com>
2021-11-23 21:07:37 +0800

    feat: 移动端菜单

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
-
 import "./index.scss";
 
 const IndexPage: React.FC = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
-    <div className="header">
+    <div
+      className="header"
+      style={{
+        overflow: showMenu ? "visible" : "hidden",
+      }}
+    >
       <div className="header-box">
         <div className="header-logo"></div>
-        <div className="header-nav">
+        {showMenu && (
+          <div
+            className="nav-mask"
+            onClick={() => setShowMenu(!showMenu)}
+          ></div>
+        )}
+
+        <div
+          className={`header-nav ${showMenu && "show-menu"}`}
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          <div className="nav-logo">
+            <div className="nav-logo-overlay"></div>
+          </div>
           <div className="nav-btn nav-home">首页</div>
           <div className="nav-btn">关于shopastro</div>
-          <div className="nav-btn">加入我们</div>
+          <div
+            className="nav-btn"
+            onClick={() => {
+              window.scrollTo({
+                top: 10000,
+                behavior: "smooth",
+              });
+            }}
+          >
+            加入我们
+          </div>
 
           <Button className="nav-btn content-us-btn">联系我们</Button>
-        </div>
-        <div className="menu-icon">
-          
-        </div>
-      </div>
 
+          <div
+            className="nav-close"
+            onClick={() => setShowMenu(!showMenu)}
+          ></div>
+        </div>
+        <div className="menu-icon" onClick={() => setShowMenu(!showMenu)}></div>
+      </div>
     </div>
   );
 };
--- src/pages/Index/src/components/Footer/index.tsx
+++ src/pages/Index/src/components/Footer/index.tsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import Title from "@components/common/Title";
 
 const Vision: React.FC = () => {
+  const [close, setClose] = useState({
+    about: true,
+    join: true,
+    content: true,
+  });
+
   return (
-    <div className="home-footer">
+    <div className="home-footer" id="homeFooter">
       <div className="footer-box">
         <div className="footer-left">
           <div className="footer-logo"></div>
@@ -24,9 +30,21 @@ const Vision: React.FC = () => {
                 <div className="item-title">About us </div>
                 <div className="item-title">关于我们</div>
               </div>
+
+              <i
+                className={`add ${close.about && "line"}`}
+                onClick={() =>
+                  setClose({
+                    ...close,
+                    about: !close.about,
+                  })
+                }
+              />
             </Title>
-            <div className="item">隐私政策</div>
-            <div className="item">用户协议</div>
+            <div className={`${close.about ? "opened" : "closeed"}`}>
+              <div className="item">隐私政策</div>
+              <div className="item">用户协议</div>
+            </div>
           </div>
           <div className="right-cell">
             <Title size={14}>
@@ -34,9 +52,35 @@ const Vision: React.FC = () => {
                 <div className="item-title">Join us</div>
                 <div className="item-title">加入我们</div>
               </div>
+              <i
+                className={`add ${close.join && "line"}`}
+                onClick={() =>
+                  setClose({
+                    ...close,
+                    join: !close.join,
+                  })
+                }
+              />
             </Title>
-            <div className="item">在招岗位</div>
-            <div className="item">投递简历</div>
+
+            <div className={`${close.join ? "opened" : "closeed"}`}>
+              <a
+                className="item"
+                href="https://www.zhipin.com/gongsi/0c4a441aa82110141nx82Nm9EFI~.html"
+                rel="noreferrer"
+                target="_blank"
+              >
+                在招岗位
+              </a>
+              <a
+                className="item"
+                href="https://www.zhipin.com/gongsi/0c4a441aa82110141nx82Nm9EFI~.html"
+                rel="noreferrer"
+                target="_blank"
+              >
+                投递简历
+              </a>
+            </div>
           </div>
           <div className="right-cell">
             <Title size={14}>
@@ -44,10 +88,28 @@ const Vision: React.FC = () => {
                 <div className="item-title">Contact us</div>
                 <div className="item-title">联系我们</div>
               </div>
+
+              <i
+                className={`add ${close.content && "line"}`}
+                onClick={() =>
+                  setClose({
+                    ...close,
+                    content: !close.content,
+                  })
+                }
+              />
             </Title>
-            <div className="item">12345678</div>
-            <div className="item">shopastro@shopastro-inc.com</div>
-            <div className="item">杭州市余杭区时代未来城5号楼1702室</div>
+            <div className={`${close.content ? "opened" : "closeed"}`}>
+              <a
+                className="item"
+                href="mailto:shopastro@shopastro-inc.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                shopastro@shopastro-inc.com
+              </a>
+              <div className="item">杭州市余杭区时代未来城5号楼1702室</div>
+            </div>
           </div>
 
           <div className="right-cell right-cell-mb">
