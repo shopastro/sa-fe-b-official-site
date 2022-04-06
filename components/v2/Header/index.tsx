757f8146fa02d473c02d35d3838f825eecb36b1a
xilei.y <yann.yxl@shopastro-inc.com>
2022-04-06 09:38:47 +0800

    fix: 打点方法优化

--- components/v2/Header/index.tsx
+++ components/v2/Header/index.tsx
@@ -4,18 +4,19 @@ import Head from 'next/head'
 type IProps = {}
 
 const Header: React.FC<IProps> = (props) => {
-  useEffect(() => {
-    ;(function (w: Record<string, any>, d, s, l: string, i) {
-      w[l] = w[l] || []
-      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
+  const sp = (w: Record<string, any>, d: Document, s: string, l: string, i: string) => {
+    w[l] = w[l] || []
+    w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' })
 
-      let f: any = d.getElementsByTagName(s)[0],
-        j: any = d.createElement(s),
-        dl = l !== 'dataLayer' ? '&l=' + l : ''
-      j.async = true
-      j.src = 'https://tagging.ishopastro.com/gtm.js?id=' + i + dl
-      f.parentNode.insertBefore(j, f)
-    })(window, document, 'script', 'dataLayer', 'GTM-TLZJ6RL')
+    let f: any = d.getElementsByTagName(s)[0],
+      j: any = d.createElement(s),
+      dl = l !== 'dataLayer' ? '&l=' + l : ''
+    j.async = true
+    j.src = 'https://tagging.ishopastro.com/gtm.js?id=' + i + dl
+    f.parentNode.insertBefore(j, f)
+  }
+  useEffect(() => {
+    sp(window, document, 'script', 'dataLayer', 'GTM-TLZJ6RL')
   })
   return (
     <Head>
