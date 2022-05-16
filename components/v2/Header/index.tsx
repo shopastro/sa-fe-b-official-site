b9c12efbdc72c0386eb1e19995ecfaf3c98d4cb1
liuyihcun66 <yicn.lyc@shopastro-inc.com>
2022-05-16 19:19:09 +0800

    feat: google埋点

--- components/v2/Header/index.tsx
+++ components/v2/Header/index.tsx
@@ -25,15 +25,15 @@ const Header: React.FC<IProps> = (props) => {
       </Head>
 
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-6WMHV7EMED" strategy="afterInteractive" />
-      <Script id="google-analytics" strategy="afterInteractive">
+      {/* <Script id="google-analytics" strategy="afterInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-6WMHV7EMED');
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://tagging.ishopastro.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TLZJ6RL')
         `}
-      </Script>
+      </Script> */}
 
       <Script id="uDesk-analytics" strategy="afterInteractive">
         {`(function(a,h,c,b,f,g){a["UdeskApiObject"]=f;a[f]=a[f]||function(){(a[f].d=a[f].d||[]).push(arguments)};g=h.createElement(c);g.async=1;g.charset="utf-8";g.src=b;c=h.getElementsByTagName(c)[0];c.parentNode.insertBefore(g,c)})(window,document,"script","https://assets-cli.s4.udesk.cn/im_client/js/udeskApi.js","ud");
@@ -44,6 +44,14 @@ const Header: React.FC<IProps> = (props) => {
             });
         `}
       </Script>
+      <noscript>
+        <iframe
+          src="https://tagging.ishopastro.com/ns.html?id=GTM-TLZJ6RL"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
     </>
   )
 }
