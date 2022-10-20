cdd53df1481b9f444a847435143245eaff1e8b4f
yefanglin <fanglin.yfl@shopastro-inc.com>
2022-10-20 20:47:21 +0800

    feat: fix desc

--- components/v2/Header/index.tsx
+++ components/v2/Header/index.tsx
@@ -21,7 +21,7 @@ const Header: React.FC<IProps> = (props) => {
         <title>shopastro 星盘跨境 一站式跨境电商saas服务平台，跨境卖家的出海首选！</title>
         <meta
           name="description"
-          content="shopastro-星盘跨境，星盘旗下一站式跨境电商Saas服务平台，为跨境卖家从获客、建站、投放、下单、物流、支付提供一站式解决方案，并有专业的服务团队及服务资源，一对一量身提供专业运营服务，是跨境卖家出海路上最值得信赖的伙伴！shopastro-星盘跨境"
+          content="shopastro，星盘跨境，星盘旗下一站式跨境电商Saas服务平台，为跨境卖家从获客、建站、投放、下单、物流、支付提供一站式解决方案，并有专业的服务团队及服务资源，一对一量身提供专业运营服务，是跨境卖家出海路上最值得信赖的伙伴！--shopastro星盘跨境"
         />
         <meta
           name="keywords"
