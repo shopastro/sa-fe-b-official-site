22a37e9f2f23cec810e47ab779ad76f489d7a373
weifeng.zwf <weifeng.zwf@shopastro-inc.com>
2023-01-05 16:17:08 +0800

    feat: form 接口

--- utils/asios.ts
+++ utils/asios.ts
@@ -1,7 +1,11 @@
 import axios from 'axios'
-const url = '//sys.api.ishopastro.com/shop/v1/partner/create.json'
+
+const prodUrl = '//sys.api.ishopastro.com/shop/v1/partner/create.json'
+const betaUrl = '//sys.api.beta.ishopastro.com/shop/v1/partner/create.json'
 
 export const axiosFun = (data = {}, callback: (p: any) => void) => {
+  const url = /beta/.test(location.host) ? betaUrl : prodUrl
+
   return axios
     .post(url, data)
     .then((response) => {
