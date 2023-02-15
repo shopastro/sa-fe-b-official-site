a831fd6cfb640303f30b41111e8d08496d8d30c8
liuyihcun66 <yicn.lyc@shopastro-inc.com>
2023-02-15 21:08:25 +0800

    fix: 增加window

--- utils/freeTrail.ts
+++ utils/freeTrail.ts
@@ -1,7 +1,7 @@
 export const freeLink = (type: string, origin: string): string => {
   let link = `https://sys.admin.ishopastro.com/admin/user/signup?product=${type}&origin=https://www.shopastro.com${origin}`
 
-  if (/beta/.test(location.host) || /localhost/.test(location.host)) {
+  if (/beta/.test(window.location.host) || /localhost/.test(window.location.host)) {
     link = `https://sys.admin.beta.ishopastro.com/admin/user/signup?product=${type}&origin=https://www.shopastro.com${origin}`
   }
 
