9133d49f2fea8d4834e1fab3bf6b8264b5da239f
He Peng <peng.hep@shopastro-inc.com>
2023-02-21 14:09:51 +0800

    feat: 增加arms 打点

---
+++ pages/_document.tsx
@@ -0,0 +1,32 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  !(function(c,b,d,a){c[a]||(c[a]={});c[a].config=
+  {
+    pid:"fagt864e6o@f960a628a01a912",
+    appType:"web",
+    imgUrl:"https://arms-retcode.aliyuncs.com/r.png?",
+    sendResource:true,
+    enableLinkTrace:true,
+    behavior:true,
+    enableSPA:true,
+    useFmp:true
+  };
+with(b)with(body)with(insertBefore(createElement("script"),firstChild))setAttribute("crossorigin","",src=d)
+})(window,document,"https://retcode.alicdn.com/retcode/bl.js","__bl");
+        `
+          }}
+        ></script>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
