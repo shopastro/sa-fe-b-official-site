564f49f59cbf1f2b8df5c8cc94384bfe1e3b4bc2
liuyihcun66 <yicn.lyc@shopastro-inc.com>
2023-02-23 19:49:39 +0800

    fix: 删除多余注释

--- utils/freeTrail.ts
+++ utils/freeTrail.ts
@@ -1,9 +1,4 @@
 export const freeLink = (type: string, origin: string): string => {
   let link = `https://sys.admin.ishopastro.com/admin/user/signup?product=${type}&origin=https://www.shopastro.com${origin}`
-
-  // if (host.includes('beta') || host.includes('localhost')) {
-  //   link = `https://sys.admin.beta.ishopastro.com/admin/user/signup?product=${type}&origin=https://www.shopastro.com${origin}`
-  // }
-
   return link
 }
