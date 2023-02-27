b2fa73173b8650f72538dfa2f1d7aa9f09e28147
liuyihcun66 <yicn.lyc@shopastro-inc.com>
2023-02-27 17:28:36 +0800

    fix: 跳转地址更改

--- utils/freeTrail.ts
+++ utils/freeTrail.ts
@@ -1,7 +1,7 @@
 export const freeLink = (type: string, origin?: string): string => {
-  let link = `https://sys.admin.ishopastro.com/admin/user/signup?product=${type}`
+  let link = `https://sys.admin.ishopastro.com/admin/user/store-login?product=${type}`
   if (origin) {
-    link = `https://sys.admin.ishopastro.com/admin/user/signup?product=${type}`
+    link = `https://sys.admin.ishopastro.com/admin/user/store-login?product=${type}`
   }
 
   return link
