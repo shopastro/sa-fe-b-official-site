0a6d72d6aeab61b344ba488c5181b8663eab9e71
liuyihcun66 <yicn.lyc@shopastro-inc.com>
2023-03-09 10:46:02 +0800

    fix: input校验

--- components/v5/Industry/Banner/index.tsx
+++ components/v5/Industry/Banner/index.tsx
@@ -10,15 +10,18 @@ const BelugaBanner = () => {
       {bannerData.map((el, i) => {
         return (
           <div
-            className={`md:flex justify-center px-[20px] md:px-[120px] py-[40px] md:py-[100px] bg-[${
+            className={`md:flex ${
+              i % 2 === 1 ? 'flex-row-reverse' : 'row'
+            } justify-center  items-center px-[20px] md:px-[120px] py-[40px] md:py-[100px] bg-[${
               i % 2 === 1 ? '#F5F6FA' : '#fff'
             }]`}
             key={el.type}
           >
-            <div className="md:w-[689px] md:h-[294px] md:w-[335px] md:h-[223px] mb-[24px] md:mb-[0]">
+            <div className="md:w-[689px] md:h-[294px] w-[325px] h-[223px] mb-[24px] md:mb-[0]">
               <Image src={el.image} height={294} width={689} alt={el.name} />
             </div>
-            <div className="md:ml-[82px] md:w-[430px]">
+
+            <div className={` ${i % 2 === 1 ? 'md:mr-[82px]' : 'md:ml-[82px]'} md:w-[430px]`}>
               <h3 className="md:font-[600] font-[500] text-[#0E1E46] md:text-[40px] text-[32px] mb-[16px] md:mb-[24px]">
                 {el.title}
               </h3>
