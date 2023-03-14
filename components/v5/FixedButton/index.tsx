15a22fbae26a2b411e9f5aa39b3936944d1cbf47
liuyihcun66 <yicn.lyc@shopastro-inc.com>
2023-03-14 09:51:05 +0800

    fix: fixed btn background

--- components/v5/FixedButton/index.tsx
+++ components/v5/FixedButton/index.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const FixedButton: React.FC<FreeButtonProps> = (props) => {
   const Button = (
     <div className="flex items-center fixed bottom-0 border-t-[1px] border-[#DBDBDB] left-0 right-0 px-[20px] py-[10px] bg-white cursor-pointer z-20 md:hidden">
-      <span className="flex-1 py-[8px] text-[18px] leading-[26px] text-[#FFF] text-center font-[700] bg-[#FE8953] rounded-[8px]">
+      <span className="flex-1 py-[8px] text-[18px] leading-[26px] text-[#FFF] text-center font-[700] bg-[#FF793A] rounded-[8px]">
         {props.text ?? '免费试用'}
       </span>
     </div>
