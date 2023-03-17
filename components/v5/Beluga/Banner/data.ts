e7a74330d1e887a47ef0bdb3a5d3e48eb8278035
liuyihcun66 <yicn.lyc@shopastro-inc.com>
2023-03-17 17:49:21 +0800

    fix: 增加文案大师入口

--- components/v5/Beluga/Banner/data.ts
+++ components/v5/Beluga/Banner/data.ts
@@ -9,13 +9,21 @@ export const bannerData = [
   },
   {
     type: '功能二',
+    title: '文案大师',
+    info: '从点子到文案一键生成！丰富的文案生成模板，覆盖跨境出海商家的全场景，涉及商品描述、博客灵感、广告文案等，集成最新GPT AI模型，10倍提升人效，内容转化率更高。',
+    infoMobile: '一键生成内容文案，提效率、促转化',
+    image: 'https://media.cdn.ishopastro.com/svg/sa-fe-b-background/copyMaster.svg',
+    name: 'beluga_copymaster'
+  },
+  {
+    type: '功能三',
     title: '白鲸广告管理',
     info: '全平台广告数据精准数据上报，提供精细化多维度的广告大数据分析，支持超大规模SKU总数和高成功率GMC和Catalog目录的商品刊登，多种手段帮助从源头上提高广告成效ROAS。',
     infoMobile: '广告精准数据上报&归因，商品高成功率刊登',
     image: 'https://media.cdn.ishopastro.com/upload/images/Frame 627453.png_2022-12-20-08-30-06.png'
   },
   {
-    type: '功能三',
+    type: '功能四',
     title: '个性化会员管理',
     info: '无上限客户记录数，行为轨迹提供精准分析能力，针对大数据自动人群和自定义圈选的人群，通过高送达率的邮件发送、短信系统可进行私域触达，从而实现真正意义上的数据化精准营销。',
     infoMobile: '高效邮件和短信触达',
