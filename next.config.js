29d86451f98f6ef996329a0b2274578e8b9eb22b
weifeng.zwf <weifeng.zwf@shopastro-inc.com>
2023-03-17 14:36:55 +0800

    feat: rewrite sw

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const BFF_URL = process.env.bff_url
+const isBeta = process.env.APP_ENV === 'dev'
 
 module.exports = {
   reactStrictMode: true,
@@ -9,6 +10,10 @@ module.exports = {
   rewrites: async () => {
     return {
       fallback: [
+        {
+          source: '/sw.js',
+          destination: isBeta ? '/sw/beta.js' : '/sw/prod.js',
+        },
         {
           source: '/api/:path*',
           destination: `${BFF_URL}/:path*`
