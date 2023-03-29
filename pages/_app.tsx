d7140e69d58e0cd2a00429f9dbb653f9ee967b4f
weifeng.zwf <weifeng.zwf@shopastro-inc.com>
2023-03-29 20:25:12 +0800

    feat: 移除无用的代码

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import 'tailwindcss/tailwind.css'
 
-import '../components/biz/Message/message.min.css'
 import DetectionStore from '../store/detectionStore'
 import '../styles/globals.css'
 
